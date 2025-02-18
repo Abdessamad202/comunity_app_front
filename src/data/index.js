e7c0a998925be9cba0f7a7f886535a4744a670e3
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2025-02-18 18:43:19 +0100

    Add src/data/index.js: Initial data file

---
+++ src/data/index.js
@@ -0,0 +1,43 @@
+import { BookOpen, Calendar, MessageCircle, Users } from "lucide-react";
+
+export const cardData = [
+  {
+    icon: Users,
+    title: "Active Members",
+    description: "Connect with passionate individuals from around the world.",
+    img: "https://images.unsplash.com/photo-1522071820081-009f0129c71c?q=80&w=2070&auto=format&fit=crop",
+  },
+  {
+    icon: MessageCircle,
+    title: "Discussions",
+    description: "Engage in meaningful conversations about topics that matter.",
+    img: "https://images.unsplash.com/photo-1530099486328-e021101a494a?q=80&w=2047&auto=format&fit=crop",
+  },
+  {
+    icon: Calendar,
+    title: "Events",
+    description: "Participate in regular meetups, workshops, and conferences.",
+    img: "https://images.unsplash.com/photo-1505373877841-8d25f7d46678?q=80&w=2012&auto=format&fit=crop",
+  },
+  {
+    icon: BookOpen,
+    title: "Resources",
+    description: "Access a wealth of knowledge and learning materials.",
+    img: "https://images.unsplash.com/photo-1517048676732-d65bc937f952?q=80&w=2070&auto=format&fit=crop",
+  },
+];
+
+export const statsData = [
+  {
+    title: "Active Members",
+    value: "10K+",
+  },
+  {
+    title: "Daily Discussions",
+    value: "5K+",
+  },
+  {
+    title: "Monthly Events",
+    value: "100+",
+  },
+];
