fc1da4073e76b3a1b5a4656a72a5e800184d4511
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2025-02-18 18:43:20 +0100

    Add src/components/CTASection.jsx: Initial call to action section component

---
+++ src/components/CTASection.jsx
@@ -0,0 +1,48 @@
+import { Rocket, User } from "lucide-react";
+import { Link } from "react-router";
+
+const CTASection = () => {
+  return (
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-20">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 items-center">
+        <div className="rounded-xl overflow-hidden">
+          <img
+            src="https://images.unsplash.com/photo-1543269865-cbf427effbad?q=80&w=2070&auto=format&fit=crop"
+            alt="Community members"
+            className="w-full h-[400px] object-cover"
+          />
+        </div>
+        <div className="text-center lg:text-left">
+          <h2 className="text-3xl font-bold text-gray-900 mb-4">
+            Ready to Join Our Community?
+          </h2>
+          <p className="text-xl text-gray-600 mb-8">
+            Start your journey today and connect with like-minded individuals who share your passions and interests.
+          </p>
+          <div className="flex space-x-4 items-center justify-center lg:justify-start">
+            <Link
+              to={'/login'}
+              className="flex px-6 py-3 bg-white text-indigo-600 border border-indigo-600 items-center gap-2 rounded-lg transition duration-200 shadow-md hover:bg-indigo-700 hover:border-white hover:text-white"
+            >
+              {/* "Get Started" icon */}
+              <Rocket className="w-5 h-5" />
+              Get Started
+            </Link>
+
+            <Link
+              to={'/register'}
+              className="flex px-6 py-3 bg-indigo-600 text-white border border-indigo-600 items-center gap-2 rounded-lg transition duration-200 shadow-md hover:bg-white hover:text-indigo-600 hover:border-indigo-600"
+            >
+              {/* "Sign Up" icon */}
+              <User className="w-5 h-5" />
+              Regiser
+            </Link>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default CTASection;
