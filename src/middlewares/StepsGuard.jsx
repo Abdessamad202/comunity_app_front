3eee5b403ce7e7f6476f81010cbada3bc8add516
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2025-02-18 02:23:26 +0100

    Update src/middlewares/StepsGuard.jsx: Added new route checks and modified existing checks

--- src/middlewares/StepsGuard.jsx
+++ src/middlewares/StepsGuard.jsx
@@ -1,28 +1,38 @@
-import { useEffect } from "react";
+import { useContext, useEffect } from "react";
 import { useNavigate, Outlet } from "react-router";
+import { UserContext } from "../context/UserContext";
 
 const StepsGuard = () => {
   const navigate = useNavigate(); // Hook to navigate programmatically
-
+  const {user} = useContext(UserContext);
   useEffect(() => {
     // Retrieve user data from localStorage
-    const user = JSON.parse(localStorage.getItem("user")) || {};
-    const token = user.token;
-    const step = user.step;
+    const token = user?.token;
+    const step = user?.step;
+    const profileId = user?.profileId;
 
     // Check if the user is logged in
-    if (!token) {
+    if (!token || !step) {
       navigate("/register"); // Redirect to registration if no token
-    } else if (step === 1) {
-      navigate("/verify"); // Redirect to verification if step 1
-    } else if (step === 2) {
-      navigate("/profile"); // Redirect to profile if step 2
-    } else {
+    } else if (profileId) {
       navigate("/home"); // Redirect to home if all steps are complete
+    } else {
+      switch (step) {
+        case 1:
+          navigate("/verify"); // Redirect to verification if step 1
+          break;
+        case 2:
+          navigate("/profile"); // Redirect to profile if step 2
+          break;
+        default:
+          navigate("/home"); // Redirect to home if all steps are complete
+          break;
+      }
+
     }
-  }, [navigate]); // Runs the effect whenever `navigate` changes
+  }, [navigate, user]); // Runs the effect whenever `navigate` changes
 
   return <Outlet />; // Render the matched route component if no redirection
 };
 
-export { StepsGuard };
+export default StepsGuard;
