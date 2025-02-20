103aaf2dc6f6c75699b2472ab873a1a82722c56f
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2025-02-20 02:47:08 +0100

    Update src/data/index.js: Updated data for components

--- src/data/index.js
+++ src/data/index.js
@@ -5,25 +5,25 @@ export const cardData = [
     icon: Users,
     title: "Active Members",
     description: "Connect with passionate individuals from around the world.",
-    img: "https://images.unsplash.com/photo-1522071820081-009f0129c71c?q=80&w=2070&auto=format&fit=crop",
+    img: "http://localhost:8000/imgs/cards/pic1.jpg",
   },
   {
     icon: MessageCircle,
     title: "Discussions",
     description: "Engage in meaningful conversations about topics that matter.",
-    img: "https://images.unsplash.com/photo-1530099486328-e021101a494a?q=80&w=2047&auto=format&fit=crop",
+    img: "http://localhost:8000/imgs/cards/pic2.jpg",
   },
   {
     icon: Calendar,
     title: "Events",
     description: "Participate in regular meetups, workshops, and conferences.",
-    img: "https://images.unsplash.com/photo-1505373877841-8d25f7d46678?q=80&w=2012&auto=format&fit=crop",
-  },
+    img: "http://localhost:8000/imgs/cards/pic3.jpg",
+    },
   {
     icon: BookOpen,
     title: "Resources",
     description: "Access a wealth of knowledge and learning materials.",
-    img: "https://images.unsplash.com/photo-1517048676732-d65bc937f952?q=80&w=2070&auto=format&fit=crop",
+    img: "http://localhost:8000/imgs/cards/pic4.jpg",
   },
 ];
 
