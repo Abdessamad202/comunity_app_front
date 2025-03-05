34a495d4a5fc67d4968b96f7bab6a6a0e6902298
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2025-03-05 22:10:54 +0000

    Update src/utils/handlers.js: [Describe the changes. E.g., Added new event handlers, modified existing handlers]

--- src/utils/handlers.js
+++ src/utils/handlers.js
@@ -1,4 +1,4 @@
-export const handleInputChange = (e, setFormData , setErrors) => {
+export const handleInputChange = (e, setFormData, setErrors) => {
   const { name, value } = e.target;
   setFormData((prevFormData) => {
     return {
